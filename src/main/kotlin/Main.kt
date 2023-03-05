dcf0c8fed65a626a5d5ec322c4a56ff35a3eedb6
Anton Trifonov <royfokker.moto@gmail.com>
2023-03-05 11:18:39 +0400

    code formatting fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 package ru.netology
+
 fun main() {
     var likes = 21
     val person = " человеку"
@@ -6,7 +7,7 @@ fun main() {
     var shownMessage = "Понравилось " + likes
     shownMessage += if (likes == 11) {
         people
-    }else if(likes == 1 || (likes - 1) % 10 == 0 )  {
+    } else if (likes == 1 || (likes - 1) % 10 == 0) {
         person
     } else {
         people
